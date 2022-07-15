fb345cc283ccb8e165dccf19ebdfbc008e87b629
Robert Curtin <robertecurtin@gmail.com>
2022-07-15 14:51:05 -0400

    This has labels

--- src/Spirits.js
+++ src/Spirits.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { Chart } from 'devextreme-react';
+import { CommonSeriesSettings, SeriesTemplate } from 'devextreme-react/chart';
+import { Legend } from 'devextreme-react/bar-gauge';
 
 const spirits = require('./config/spirits.json');
 
@@ -20,6 +23,24 @@ const sortByOffenseAscending = (a, b) => {
   return b.summary[0] - a.summary[0];
 };
 
+const summaryToBarData = (s) => {
+  return [
+    { label: "Offense", value: s[0]},
+    { label: "Control", value: s[1]},
+    { label: "Fear", value: s[2]},
+    { label: "Defense", value: s[3]},
+    { label: "Utility", value: s[4]},
+  ];
+};
+
+const palette = [
+  '#ED5564',
+  '#4FC1E8',
+  '#AC92EB',
+  '#A0D568',
+  '#FFCE54',
+];
+
 function Spirits() {
   const sortedSpirits = spirits.sort(sortByOffenseAscending);
 
@@ -31,11 +52,23 @@ function Spirits() {
             <CardActionArea>
               <CardMedia
                 component="img"
-                height="140"
                 image={spiritToImage(spirit.name)}
                 alt= {spirit.name}
                               />
               <CardContent>
+                <Chart
+                  palette={palette}
+                  dataSource={summaryToBarData(spirit.summary)}>
+
+                  <CommonSeriesSettings
+                    argumentField="label"
+                    valueField="value"
+                    type="bar"
+                    ignoreEmptyPoints={true}
+                  />
+                  <SeriesTemplate nameField="label" />
+                  <Legend visible={false} />
+                </Chart>
                 <Typography gutterBottom variant="h5" component="div">
                   {spirit.name}
                 </Typography>
